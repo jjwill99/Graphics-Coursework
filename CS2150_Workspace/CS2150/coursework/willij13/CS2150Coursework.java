85785855a7ed184bfccb6b2a01154b9032889445
Jacob <jjwill@hotmail.co.uk>
2018-12-01 23:08:23 +0000

    Bar added

--- CS2150_Workspace/CS2150/coursework/willij13/CS2150Coursework.java
+++ CS2150_Workspace/CS2150/coursework/willij13/CS2150Coursework.java
@@ -1,5 +1,5 @@
 /* CS2150Coursework.java
- * TODO: Aston University. Vivek Bhukhan, Devin Shingadia, Jacob Williams
+ * TODO: Aston University. Jacob Williams
  *
  * Scene Graph:
  *  Scene origin
@@ -80,6 +80,14 @@ public class CS2150Coursework extends GraphicsLab {
 	 * display list id of the ceiling light
 	 */
 	private final int lightList = 10;
+	/**
+	 * display list id of the bar
+	 */
+	private final int barList = 11;
+	/**
+	 * display list id of the shelf
+	 */
+	private final int shelfList = 12;
 
 	private float ringAngle = 0.0f;
 
@@ -114,6 +122,8 @@ public class CS2150Coursework extends GraphicsLab {
 
 	private Texture ceilingLightTextures;
 
+	private Texture barSideTextures;
+
 	// TODO: Feel free to change the window title and default animation scale
 	// here
 	public static void main(String args[]) {
@@ -128,6 +138,7 @@ public class CS2150Coursework extends GraphicsLab {
 		tableTextures = loadTexture("coursework/willij13/textures/table_top.jpg");
 		ceilingTextures = loadTexture("coursework/willij13/textures/ceiling.jpg");
 		ceilingLightTextures = loadTexture("coursework/willij13/textures/light_panel.png");
+		barSideTextures = loadTexture("coursework/willij13/textures/bar_side.jpg");
 
 		// global ambient light level
 		float globalAmbient[] = { 0.8f, 0.8f, 0.8f, 1.0f }; // set the global ambient lighting
@@ -205,16 +216,26 @@ public class CS2150Coursework extends GraphicsLab {
 			drawUnitLight();
 		}
 		GL11.glEndList();
+		GL11.glNewList(barList, GL11.GL_COMPILE);
+		{
+			drawUnitBar();
+		}
+		GL11.glEndList();
+		GL11.glNewList(shelfList, GL11.GL_COMPILE);
+		{
+			drawUnitShelf();
+		}
+		GL11.glEndList();
 	}
 
 	protected void checkSceneInput() {
 		if (thrown == true) {
 			if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
 				dartAngleY = 10.0f;
-				dartMovementY += 0.01f;
+				dartMovementY += 0.001f;
 				if (dartYLimit()) {
 					dartMovementY = 1.8f;
-					heightView -= 0.01f;
+					heightView -= 0.001f;
 					if (heightView < -5.5) {
 						dartAngleY = 0.0f;
 						heightView = -5.5f;
@@ -222,10 +243,10 @@ public class CS2150Coursework extends GraphicsLab {
 				}
 			} else if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
 				dartAngleY = -10.0f;
-				dartMovementY -= 0.01f;
+				dartMovementY -= 0.001f;
 				if (dartYLimit()) {
 					dartMovementY = -0.6f;
-					heightView += 0.01f;
+					heightView += 0.001f;
 					if (heightView > 0.0f) {
 						dartAngleY = 0.0f;
 						heightView = 0.0f;
@@ -233,10 +254,10 @@ public class CS2150Coursework extends GraphicsLab {
 				}
 			} else if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
 				dartAngleX = 5.0f;
-				dartMovementX -= 0.01f;
+				dartMovementX -= 0.001f;
 				if (dartXLimit()) {
 					dartMovementX = -1.8f;
-					widthView += 0.01f;
+					widthView += 0.001f;
 					if (widthView > 5.5) {
 						dartAngleX = 0.0f;
 						widthView = 5.5f;
@@ -244,10 +265,10 @@ public class CS2150Coursework extends GraphicsLab {
 				}
 			} else if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
 				dartAngleX = -5.0f;
-				dartMovementX += 0.01f;
+				dartMovementX += 0.001f;
 				if (dartXLimit()) {
 					dartMovementX = 1.8f;
-					widthView -= 0.01f;
+					widthView -= 0.001f;
 					if (widthView < -5.5) {
 						dartAngleX = 0.0f;
 						widthView = -5.5f;
@@ -258,7 +279,7 @@ public class CS2150Coursework extends GraphicsLab {
 				dartAngleX = 0.0f;
 			}
 
-			moving += 0.01f;
+			moving += 0.001f;
 			checkDartHit();
 		} else {
 			spinRotationAngle = 45f;
@@ -286,9 +307,9 @@ public class CS2150Coursework extends GraphicsLab {
 		// can be made faster or slower depending on the machine you are working
 		// on
 
-		spinRotationAngle += +40.0f * getAnimationScale();
+		spinRotationAngle += +5.0f * getAnimationScale();
 
-		ringAngle += 10.0f * getAnimationScale();
+		ringAngle += 3.0f * getAnimationScale();
 	}
 
 	protected void renderScene() {
@@ -435,7 +456,7 @@ public class CS2150Coursework extends GraphicsLab {
 			GL11.glBindTexture(GL11.GL_TEXTURE_2D, wallTextures.getTextureID());
 
 			// position, scale and draw the back plane using its display list
-			GL11.glTranslatef(-12.5f + widthView, 4.0f + heightView, -7.5f + moving);
+			GL11.glTranslatef(-12.5f + widthView, 4.0f + heightView, -7.0f + moving);
 			GL11.glRotatef(90.0f, 1.0f, 0, 0);
 			GL11.glRotatef(270.0f, 0.0f, 0.0f, 1.0f);
 			GL11.glScalef(25.0f, 1.0f, 10.0f);
@@ -516,7 +537,7 @@ public class CS2150Coursework extends GraphicsLab {
 			GL11.glBindTexture(GL11.GL_TEXTURE_2D, wallTextures.getTextureID());
 
 			// position, scale and draw the back plane using its display list
-			GL11.glTranslatef(12.5f + widthView, 4.0f + heightView, -7.5f + moving);
+			GL11.glTranslatef(12.5f + widthView, 4.0f + heightView, -7.0f + moving);
 			GL11.glRotatef(90.0f, 1.0f, 0, 0);
 			GL11.glRotatef(90.0f, 0.0f, 0.0f, 1.0f);
 			GL11.glScalef(25.0f, 1.0f, 10.0f);
@@ -778,6 +799,31 @@ public class CS2150Coursework extends GraphicsLab {
 				GL11.glCallList(ringList);
 			}
 			GL11.glPopMatrix();
+		}
+		GL11.glPopMatrix();
+
+		// Renders the bar
+		GL11.glPushMatrix();
+		{
+			// disable lighting calculations so that they don't affect
+			// the appearance of the texture
+			GL11.glPushAttrib(GL11.GL_LIGHTING_BIT);
+			GL11.glDisable(GL11.GL_LIGHTING);
+			// change the geometry colour to white so that the texture
+			// is bright and details can be seen clearly
+			Colour.WHITE.submit();
+			// enable texturing and bind an appropriate texture
+			GL11.glEnable(GL11.GL_TEXTURE_2D);
+			GL11.glBindTexture(GL11.GL_TEXTURE_2D, barSideTextures.getTextureID());
+
+			GL11.glTranslatef(10 + widthView, 0.5f + heightView, -25 + moving);
+			GL11.glRotatef(270, 0, 1, 0);
+
+			GL11.glCallList(barList);
+
+			// disable textures and reset any local lighting changes
+			GL11.glDisable(GL11.GL_TEXTURE_2D);
+			GL11.glPopAttrib();
 		}
 		GL11.glPopMatrix();
 
@@ -1543,6 +1589,96 @@ public class CS2150Coursework extends GraphicsLab {
 			v8.submit();
 		}
 		GL11.glEnd();
+	}
+
+	private void drawUnitBar() {
+		Vertex v1 = new Vertex(-25.0f, 1.5f, 1.0f);
+		Vertex v2 = new Vertex(25.0f, 1.5f, 1.0f);
+		Vertex v3 = new Vertex(-25.0f, -1.5f, 1.0f);
+		Vertex v4 = new Vertex(25.0f, -1.5f, 1.0f);
+		Vertex v5 = new Vertex(-25.0f, 1.5f, -1.0f);
+		Vertex v6 = new Vertex(25.0f, 1.5f, -1.0f);
+		Vertex v7 = new Vertex(-25.0f, -1.5f, -1.0f);
+		Vertex v8 = new Vertex(25.0f, -1.5f, -1.0f);
+
+		// draw the near face:
+		GL11.glBegin(GL11.GL_POLYGON);
+		{
+			new Normal(v3.toVector(), v4.toVector(), v2.toVector(), v1.toVector()).submit();
+
+			GL11.glTexCoord2f(0.0f, 0.0f);
+			v3.submit();
+			GL11.glTexCoord2f(10.0f, 0.0f);
+			v4.submit();
+			GL11.glTexCoord2f(10.0f, 1.0f);
+			v2.submit();
+			GL11.glTexCoord2f(0.0f, 1.0f);
+			v1.submit();
+		}
+		GL11.glEnd();
+
+		// draw the far face:
+		GL11.glBegin(GL11.GL_POLYGON);
+		{
+			new Normal(v8.toVector(), v7.toVector(), v5.toVector(), v6.toVector()).submit();
+
+			v8.submit();
+			v7.submit();
+			v5.submit();
+			v6.submit();
+		}
+		GL11.glEnd();
+
+		// draw the left face:
+		GL11.glBegin(GL11.GL_POLYGON);
+		{
+			new Normal(v1.toVector(), v5.toVector(), v7.toVector(), v3.toVector()).submit();
+
+			v1.submit();
+			v5.submit();
+			v7.submit();
+			v3.submit();
+		}
+		GL11.glEnd();
+
+		// draw the right face:
+		GL11.glBegin(GL11.GL_POLYGON);
+		{
+			new Normal(v2.toVector(), v4.toVector(), v8.toVector(), v6.toVector()).submit();
+
+			v2.submit();
+			v4.submit();
+			v8.submit();
+			v6.submit();
+		}
+		GL11.glEnd();
+
+		// draw the top face:
+		GL11.glBegin(GL11.GL_POLYGON);
+		{
+			new Normal(v1.toVector(), v2.toVector(), v6.toVector(), v5.toVector()).submit();
+
+			v1.submit();
+			v2.submit();
+			v6.submit();
+			v5.submit();
+		}
+		GL11.glEnd();
+
+		// draw the bottom face:
+		GL11.glBegin(GL11.GL_POLYGON);
+		{
+			new Normal(v4.toVector(), v3.toVector(), v7.toVector(), v8.toVector()).submit();
+
+			v4.submit();
+			v3.submit();
+			v7.submit();
+			v8.submit();
+		}
+		GL11.glEnd();
+	}
+
+	private void drawUnitShelf() {
 	}
 
 }
